4804f6576749b46b4f05ad09b9f6e08b25168197
Raghad Saleh <iraghad340@gmail.com>
2024-02-07 12:29:16 +0300

    This is test for Group

--- src/lap1/Lap1.java
+++ src/lap1/Lap1.java
@@ -7,7 +7,7 @@ public class Lap1 {
     
     public static void main(String[] args) {
        System.out.println("hi im shroug Alharbi");
-        System.out.println("Hi I'm Raghad");
+        System.out.println("Hi I'm Raghad Alotaibi");
     }
     
 }
